4bcac2d6508ef9a3d40ae1f786aa3ec208f454e6
Abai <abai2010@bk.ru>
2017-12-23 12:59:11 +0600

    finished

---
+++ hw14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace hw14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person() { Name = "Alex", Age = 28 };
+            GC.Collect();
+            Console.WriteLine(GC.GetGeneration(person1));
+            GC.Collect();
+            Console.WriteLine(GC.GetGeneration(person1));
+
+            Console.ReadLine();
+        }
+    }
+}
